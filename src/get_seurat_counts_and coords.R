1dcef9cfc90031bc0743f2ca1c4321db788f5cac
maddoxav <maddoxav@med.umich.edu>
2022-06-28 16:36:55 -0400

    Modified get_seurat_counts_and coords.R

--- src/get_seurat_counts_and coords.R
+++ src/get_seurat_counts_and coords.R
@@ -22,46 +22,56 @@ get_tissue <- function(merged_seur_data, tiss_id) {
 }
 
 # create data frame of tissue coordinates
-get_tissue_coordinates <- function(seur_data){
+get_tissue_coordinates <- function(seur_data) {
   tc <- GetTissueCoordinates(seur_data)
   tc$barcode <- rownames(tc)
-  tc <- tc[,c(ncol(tc),1:(ncol(tc)-1))]
+  tc <- tc[, c(ncol(tc), 1:(ncol(tc) - 1))]
   return(tc)
 }
 
-# write each sample's assay df (just og spatial counts for now)
-# and barcode index to feather files
+# get tissue image dimensions
+get_img_dims <- function(seur_data, img_name) {
+  img <- seur_data@images[[img_name]]@image
+  return(dim(img)[c(1, 2)])
+}
+
+# write each sample's assay df
+# and barcode index to feather files, and img dims to txt
+write_data <- function(dest_dir, data_path, data_id, tissue_ids) {
+  merged_seur_data <- readRDS(data_path)
+  for (tissue_id in tissue_ids) {
+    seur_data <- get_tissue(merged_seur_data, tissue_id)
+    # get data from seurat object
+    mat_data <- assay_to_mat(seur_data, assay = "SCT", slot = "counts")
+    tc <- get_tissue_coordinates(seur_data)
+    im_dims <- get_img_dims(seur_data, tissue_id)
+    # output file names
+    md_fname <-
+      paste(tissue_id, data_id, "count_mat.feather", sep = "-")
+    tc_fname <- paste(tissue_id, data_id, "tc.feather", sep = "-")
+    im_dim_fname <-
+      paste(tissue_id, data_id, "im_dims.txt", sep = "-")
+    # output file paths
+    md_path <- file.path(dest_dir, md_fname)
+    tc_path <- file.path(dest_dir, tc_fname)
+    im_dim_path <- file.path(dest_dir, im_dim_fname)
+    # write to files
+    write_feather(mat_data, md_path)
+    write_feather(tc, tc_path)
+    cat(im_dims, file=im_dim_path, sep=" ")
+  }
+}
+
 tissue_ids <- c("brain1A", "brain1B", "brain1C", "brain1D")
 data_dir <- "Rao_Lab/data/GBM_spatial_transcriptomics"
+dest_dir <- file.path(data_dir, "assay_data")
 # sample 4774
 data_path <-
   file.path(data_dir, "Sample-4774/4774_merged.rds")
-merged_seur_data <- readRDS(data_path)
-for (tissue_id in tissue_ids) {
-  seur_data <- get_tissue(merged_seur_data, tissue_id)
-  mat_data <- assay_to_mat(seur_data)
-  tc <- get_tissue_coordinates(seur_data)
-  dest_dir <- file.path(data_dir, "assay_data")
-  md_fname <- paste(tissue_id, "s4774", "count_mat.feather", sep="-")
-  tc_fname <- paste(tissue_id, "s4774", "tc.feather", sep="-")
-  md_path <- file.path(dest_dir, md_fname)
-  tc_path <- file.path(dest_dir, tc_fname)
-  write_feather(mat_data, md_path)
-  write_feather(tc, tc_path)
-}
+data_id <- "s4774"
+write_data(dest_dir, data_path, data_id, tissue_ids)
 # sample 1512
 data_path <-
   file.path(data_dir, "Sample-1512/1512_rpca_integrated.rds")
-merged_seur_data <- readRDS(data_path)
-for (tissue_id in tissue_ids) {
-  seur_data <- get_tissue(merged_seur_data, tissue_id)
-  mat_data <- assay_to_mat(seur_data)
-  tc <- get_tissue_coordinates(seur_data)
-  dest_dir <- file.path(data_dir, "assay_data")
-  md_fname <- paste(tissue_id, "s1512", "count_mat.feather", sep="-")
-  tc_fname <- paste(tissue_id, "s1512", "tc.feather", sep="-")
-  md_path <- file.path(dest_dir, md_fname)
-  tc_path <- file.path(dest_dir, tc_fname)
-  write_feather(mat_data, md_path)
-  write_feather(tc, tc_path)
-}
+data_id <- "s1512"
+write_data(dest_dir, data_path, data_id, tissue_ids)
